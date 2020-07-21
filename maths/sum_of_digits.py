0b4cc4a14f908e332e62018f0db2c278f0dd8557
Vasu Gamdha <40864108+vasugamdha@users.noreply.github.com>
2020-07-21 15:02:19 +0530

    Updated sum_of_digits.py
    Added a new method!

--- maths/sum_of_digits.py
+++ maths/sum_of_digits.py
@@ -13,6 +13,17 @@ def sum_of_digits(n: int) -> int:
         n = n // 10
     return res
 
+def sum_of_digits2(n: int) -> int:
+    """
+    Find the sum of digits of a number.
+
+    >>> sum_of_digits2(12345)
+    15
+    >>> sum_of_digits2(123)
+    6
+    """
+    return sum(map(int, str(n).strip()))
 
 if __name__ == "__main__":
-    print(sum_of_digits(12345))  # ===> 15
+    print(sum_of_digits(12345))   # ===> 15
+    print(sum_of_digits2(12345))  # ===> 15
